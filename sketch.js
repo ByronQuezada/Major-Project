091aff5c83830a83dbdfb10a371b638b35f968f5
ByronQuezada <71724896+ByronQuezada@users.noreply.github.com>
2020-10-15 11:08:36 -0600

    moved from other repo

--- sketch.js
+++ sketch.js
@@ -1,14 +1,52 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
 
+let startButton;
+let running;
+let isMouseBeingUsed;
+let player;
+//Button Variables
 
-function preload() {
+let gameStarted = false;
 
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
 }
 
 function draw() {
-  background("black");
+  background(220);
+  image(running, mouseX, mouseY);
+  
 }
+
+function preload() {
+  running = loadImage("assets/running.png");
+  
+}
+function startGame() {
+  startButton.hide();
+  noCursor();
+  isMouseBeingUsed = true;
+  gameStarted = true;
+
+}
+//Ending game and displaying try again button
+function endGame() {
+  gameStarted = false;
+  background("black");
+  fill(255);
+  text("GAME OVER", width / 2, height /2);
+  cursor();
+  
+ 
+}
+//Displaying start button
+startButton = createButton("Start Game");
+startButton.position(width / 2 - width / 4 / 2, height / 2 - height / 8 / 2);
+startButton.size(width /4, height / 8);
+startButton.mousePressed(startGame);
